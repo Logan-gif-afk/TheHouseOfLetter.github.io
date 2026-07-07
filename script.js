9479b1e5ee96807780b9066d8da664b2833befb4
Logan-gif-afk <falconlogan84@gmail.com>
2026-07-07 12:20:29 +0300

    Add fade-in effect on DOM content loaded

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.body.style.opacity = 0;
+
+    setTimeout(() => {
+        document.body.style.transition = "opacity 0.8s";
+        document.body.style.opacity = 1;
+    },100);
+
+});
